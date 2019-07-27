e43abc190183f96582ef57e4c45393cde33e7fb8
kasozi wilson <soziandwelewilson@gmail.com>
2019-07-27 17:00:03 -0500

    server init

---
+++ server/src/app.js
@@ -0,0 +1,20 @@
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+
+const app = express()
+app.use(morgan('combined')) // prints logs; user agent; verbose logs
+app.use(bodyParser.json())
+app.use(cors()) // allow any client hit our server; will setup token later
+
+
+app.get('/status', (req, res) =>{
+  res.send({
+    message: 'hello world!'
+  })
+})
+
+app.listen(process.env.port || 8081)
